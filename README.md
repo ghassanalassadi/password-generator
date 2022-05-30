522dc67c851f21ac514c01ff85eeea30c14db277
ghassan <ghassanalassadi@outlook.com>
2022-05-30 18:09:23 +1000

    made adjustments to program to meet all criteria and adjusted README file

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# password-generator
+<h1>Password Generator</h1>
+
+<h2>Description</h2>
+This project was developed to create a simple password generator. It allows the user to select what types of characters they desire (be it special, numeric, lower or upper case) and then generate the password based on the desired length that the user can input. A tool such as this can help users generate secure passwords, while also meeting the password requirements laid out when creating accounts with banks, social media and vice versa. Thanks to this project, I learned more about how to program in JavaScript.
+
+<h2>How to Run the Application</h2>
+You can run the application by clicking this link.
+
+<h2>Screenshot</h2>
+<img src="assets/image.png">
--- script.js
+++ script.js
@@ -127,99 +127,124 @@ var allCharacters = specialCharacters + numericCharacters + lowerCasedCharacters
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var specialCheckbox = document.querySelector("#special-characters");
-var numericCheckbox = document.querySelector("#numeric-characters");
-var lowerCasedCheckbox = document.querySelector("#lower-case");
-var upperCasedCheckbox = document.querySelector("#upper-case");
 
 // Write password to the #password input
 function writePassword() {
   function generatePassword() {
     // Password criteria prompts
-    var passwordLength = prompt("Please enter the desired length of your password (between 8 and 128):");
     var passVal = "";
+    var specialCheck = confirm("Would you like special characters in your password?");
+    var numericCheck = confirm("Would you like numeric characters in your password?");
+    var lowerCasedCheck = confirm("Would you like lower case characters in your password?");
+    var upperCasedCheck = confirm("Would you like upper case characters in your password?");
 
     // Password length (must be min of 8 characters, max 128 characters)
-    if (passwordLength < 8) {
-      alert("Please enter a higher value.");
-    }
-    else if (passwordLength > 128) {
-      alert("Please enter a lower value.")
-    }
-    else if (isNaN(passwordLength) == true) {
-      alert("Please enter a number between 8 and 128.")
+    // Input must not be a non-numerical character
+    // Keep prompting user if they enter undesirable inputs
+    while (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength) == true) {
+      var passwordLength = prompt("Please enter the desired length of your password (between 8 and 128):");
+      if (passwordLength < 8) {
+        alert("Please enter a higher value.");
+      }
+      else if (passwordLength > 128) {
+        alert("Please enter a lower value.")
+      }
+      else if (isNaN(passwordLength) == true) {
+        alert("Please enter a number between 8 and 128.")
+      }
     }
 
-    // generate the password based on which checkboxes are checked
+    // generate the password based on which criteria are desired
+
     // check if special characters are desired
-    if (specialCheckbox.checked == true && numericCheckbox.checked == false && lowerCasedCheckbox.checked == false && upperCasedCheckbox.checked == false) {
+    if (specialCheck == true && numericCheck == false && lowerCasedCheck == false && upperCasedCheck == false) {
       for (index = 0, n = specialCharacters.length; index < passwordLength; index++) {
         passVal += specialCharacters.charAt(Math.random() * n);
       }
     }
     // check if numeric characters are desired
-    else if (specialCheckbox.checked == false && numericCheckbox.checked == true && lowerCasedCheckbox.checked == false && upperCasedCheckbox.checked == false) {
+    else if (specialCheck == false && numericCheck == true && lowerCasedCheck == false && upperCasedCheck == false) {
       for (index = 0, n = numericCharacters.length; index < passwordLength; index++) {
         passVal += numericCharacters.charAt(Math.random() * n);
       }
     }
     // check if lower case characters are desired
-    else if (specialCheckbox.checked == false && numericCheckbox.checked == false && lowerCasedCheckbox.checked == true && upperCasedCheckbox.checked == false) {
+    else if (specialCheck == false && numericCheck == false && lowerCasedCheck == true && upperCasedCheck == false) {
       for (index = 0, n = lowerCasedCharacters.length; index < passwordLength; index++) {
         passVal += lowerCasedCharacters.charAt(Math.random() * n);
       }
     }
     // check if upper case characters are desired
-    else if (specialCheckbox.checked == false && numericCheckbox.checked == false && lowerCasedCheckbox.checked == false && upperCasedCheckbox.checked == true) {
+    else if (specialCheck == false && numericCheck == false && lowerCasedCheck == false && upperCasedCheck == true) {
       for (index = 0, n = upperCasedCharacters.length; index < passwordLength; index++) {
         passVal += upperCasedCharacters.charAt(Math.random() * n);
       }
     }
     // check if special and numeric are desired
-    else if (specialCheckbox.checked == true && numericCheckbox.checked == true && lowerCasedCheckbox.checked == false && upperCasedCheckbox.checked == false) {
+    else if (specialCheck == true && numericCheck == true && lowerCasedCheck == false && upperCasedCheck == false) {
       for (index = 0, n = specialNumeric.length; index < passwordLength; index++) {
         passVal += specialNumeric.charAt(Math.random() * n);
       }
     }
     // check if special and lower case are desired
-    else if (specialCheckbox.checked == true && numericCheckbox.checked == false && lowerCasedCheckbox.checked == false && upperCasedCheckbox.checked == false) {
-
+    else if (specialCheck == true && numericCheck == false && lowerCasedCheck == true && upperCasedCheck == false) {
+      for (index = 0, n = specialLower.length; index < passwordLength; index++) {
+        passVal += specialLower.charAt(Math.random() * n);
+      }
     }
     // check if special and upper case are desired
-    else if (specialCheckbox.checked == true && numericCheckbox.checked == false && lowerCasedCheckbox.checked == false && upperCasedCheckbox.checked == false) {
-
+    else if (specialCheck == true && numericCheck == false && lowerCasedCheck == false && upperCasedCheck == true) {
+      for (index = 0, n = specialUpper.length; index < passwordLength; index++) {
+        passVal += specialUpper.charAt(Math.random() * n);
+      }
     }
     // check if numeric and lower are desired
-    else if (specialCheckbox.checked == true && numericCheckbox.checked == false && lowerCasedCheckbox.checked == false && upperCasedCheckbox.checked == false) {
-
+    else if (specialCheck == false && numericCheck == true && lowerCasedCheck == true && upperCasedCheck == false) {
+      for (index = 0, n = numericLower.length; index < passwordLength; index++) {
+        passVal += numericLower.charAt(Math.random() * n);
+      }
     }
     // check if numeric and upper are desired
-    else if (specialCheckbox.checked == true && numericCheckbox.checked == false && lowerCasedCheckbox.checked == false && upperCasedCheckbox.checked == false) {
-
+    else if (specialCheck == false && numericCheck == true && lowerCasedCheck == false && upperCasedCheck == true) {
+      for (index = 0, n = numericUpper.length; index < passwordLength; index++) {
+        passVal += numericUpper.charAt(Math.random() * n);
+      }
     }
     // check if lower and upper are desired
-    else if (specialCheckbox.checked == true && numericCheckbox.checked == false && lowerCasedCheckbox.checked == false && upperCasedCheckbox.checked == false) {
-
+    else if (specialCheck == false && numericCheck == false && lowerCasedCheck == true && upperCasedCheck == true) {
+      for (index = 0, n = lowerUpper.length; index < passwordLength; index++) {
+        passVal += lowerUpper.charAt(Math.random() * n);
+      }
     }
     // check if special, numeric and lower are desired
-    else if (specialCheckbox.checked == true && numericCheckbox.checked == false && lowerCasedCheckbox.checked == false && upperCasedCheckbox.checked == false) {
-
+    else if (specialCheck == true && numericCheck == true && lowerCasedCheck == true && upperCasedCheck == false) {
+      for (index = 0, n = specialNumericLower.length; index < passwordLength; index++) {
+        passVal += specialNumericLower.charAt(Math.random() * n);
+      }
     }
     // check if special, numeric and upper are desired
-    else if (specialCheckbox.checked == true && numericCheckbox.checked == false && lowerCasedCheckbox.checked == false && upperCasedCheckbox.checked == false) {
-
+    else if (specialCheck == true && numericCheck == true && lowerCasedCheck == false && upperCasedCheck == true) {
+      for (index = 0, n = specialNumericUpper.length; index < passwordLength; index++) {
+        passVal += specialNumericUpper.charAt(Math.random() * n);
+      }
     }
     // check if special, lower and upper are desired
-    else if (specialCheckbox.checked == true && numericCheckbox.checked == false && lowerCasedCheckbox.checked == false && upperCasedCheckbox.checked == false) {
-
+    else if (specialCheck == true && numericCheck == false && lowerCasedCheck == true && upperCasedCheck == true) {
+      for (index = 0, n = specialLowerUpper.length; index < passwordLength; index++) {
+        passVal += specialLowerUpper.charAt(Math.random() * n);
+      }
     }
     // check if numeric, lower and upper are desired
-    else if (specialCheckbox.checked == true && numericCheckbox.checked == false && lowerCasedCheckbox.checked == false && upperCasedCheckbox.checked == false) {
-
+    else if (specialCheck == false && numericCheck == true && lowerCasedCheck == true && upperCasedCheck == true) {
+      for (index = 0, n = numericLowerUpper.length; index < passwordLength; index++) {
+        passVal += numericLowerUpper.charAt(Math.random() * n);
+      }
     }
     // check if all character types are desired
-    else if (specialCheckbox.checked == true && numericCheckbox.checked == false && lowerCasedCheckbox.checked == false && upperCasedCheckbox.checked == false) {
-
+    else if (specialCheck == true && numericCheck == true && lowerCasedCheck == true && upperCasedCheck == true) {
+      for (index = 0, n = allCharacters.length; index < passwordLength; index++) {
+        passVal += allCharacters.charAt(Math.random() * n);
+      }
     }
 
     return passVal;
